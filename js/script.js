b811cf0c155e99902681373bc3891e8f74c90945
tharsila <tharsilapaula@gmail.com>
2022-03-07 21:18:55 -0300

    altera caminho do js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+import initMenuMobile from './js/modules/menu-mobile.js';
+import initColorLink from './js/modules/color-link.js';
+import initScrollSmooth from './js/modules/scroll-smooth.js';
+import initModal from './js/modules/modal.js';
+
+
+initMenuMobile();
+initColorLink()
+initScrollSmooth();
+initModal();
